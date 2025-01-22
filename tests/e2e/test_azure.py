076e8829081d63915cae1167906900a6403f12a0
Chris Hawes <chawes@nvidia.com>
2025-01-22 10:40:20 -0800

    test: Add Azure E2E Tests

---
+++ tests/e2e/test_azure.py
@@ -0,0 +1,27 @@
+# SPDX-FileCopyrightText: Copyright (c) 2024 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+# SPDX-License-Identifier: Apache-2.0
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import common
+
+
+def test_azure_shortcuts():
+    profiles = ["test-azure-uswest"]
+    for profile in profiles:
+        common.test_shortcuts(profile)
+
+
+def test_azure_storage_client():
+    profile = "test-azure-uswest"
+    common.test_storage_client(profile)
